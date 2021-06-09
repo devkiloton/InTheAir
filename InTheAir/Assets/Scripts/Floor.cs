7f222c1ee8e96c80baa224f6c386923396529ba7
devkiloton <dev.kiloton@gmail.com>
2021-06-08 22:22:32 -0300

    unplayable_version

---
+++ InTheAir/Assets/Scripts/Floor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    [SerializeField]
+    private float velocity;
+    private Vector2 positionImage;
+    private float realImageSize;
+    private float imageScale;
+    private void Start()
+    {
+        positionImage = transform.position;
+        realImageSize = GetComponent<SpriteRenderer>().size.x;
+        imageScale = transform.localScale.x;
+        
+    }
+    private void Update()
+    {
+        float distance = Mathf.Repeat(velocity * Time.timeSinceLevelLoad, realImageSize * imageScale);
+        transform.position = positionImage + Vector2.left * distance;
+    }
+}
